7f6a9cf75fbc7957a22ab9bf60760b03e136e95f
acmendoza11 <acemendoza1113@gmail.com>
2021-10-21 12:48:28 +0800

    Admin home Dashboard updated

--- src/pages/adminPages/AdminDashboardHome.js
+++ src/pages/adminPages/AdminDashboardHome.js
@@ -1,13 +1,62 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import '../../styles/pages/AdminDashboardHome.css'
+import { FaUserCircle } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
 
 function AdminDashboardHome() {
 
     return (
         <>
-            <Container className=" pt-5 d-flex  justify-content-center">
-                <Row className=" w-100">
+        
+            <Container className="extend-width  " >
+                <Row className=" w-100 pt-5 ">
+                    <Col lg={12} sm={12} className=" shadow " >
+                        <Row>
+                            <Col className="boxTitle p-2">
+                                <div >
+                                    <h4>Admin Information</h4>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row className="boxInfo p-2 d-flex align-items-center">
+                            <Col lg={2} sm={12}>
+                                <IconContext.Provider value={{size:"80px", color:"#710E1D"}}>
+                                    <FaUserCircle />
+                                </IconContext.Provider>
+                            </Col>
+                            
+                            <Col lg={5} sm={12}>
+                                    <p className="LName">Manabat,</p>
+                                    <p className="FName">RAPHAEL DUCUT</p>
+                            </Col>
+                            <span className="borderLeft"></span>
+                            <Col lg={2} sm={12} className="">
+                                <div>
+                                    <p className="AdminInfo m-0 p-0">Admin Number : </p>
+                                    <p className="AdminInfo  m-0 p-0">Gender: </p>
+                                    <p className="AdminInfo  m-0 p-0">Nationality: </p>
+                                    <p className="AdminInfo ">Region: </p>
+                                    
+                                </div>
+                            </Col>
+                            <Col lg={2} sm={12} className="">
+                                <div>
+                                    <p className="AdminInfo m-0 p-0">12345674</p>
+                                    <p className="AdminInfo  m-0 p-0">Male</p>
+                                    <p className="AdminInfo  m-0 p-0">Filipino</p>
+                                    <p className="AdminInfo ">Yen's Org</p>
+                                    
+                                </div>
+                            </Col>
+                            
+                            
+                        </Row>
+                    </Col>
+                </Row>
+                
+            
+                <Row className=" w-100 pt-5 justify-content-center" >
                     <Col lg={5} sm={12} className=" shadow " >
                         <Row>
                             <Col className="boxTitle p-2">
@@ -16,12 +65,12 @@ function AdminDashboardHome() {
                                 </div>
                             </Col>
                         </Row>
-                        <Row className="boxInfo p-2">
+                        <Row className="boxInfo p-2 ">
                             <Col >
                                 <div>
-                                    <p className="parCenter">Total Employees: 99</p><br/>
-                                    <p className="parCenter">Total Employees Resigned: 99</p><br/>
-                                    <p className="parCenter">Total Employees Present: 99</p><br/>
+                                    <p className="parCenter">Total Employees: 99</p><br/><br/>
+                                    <p className="parCenter">Total Employees Resigned: 99</p><br/><br/>
+                                    <p className="parCenter">Total Employees Present: 99</p>
                                 </div>
                             </Col>
                         </Row>
@@ -37,21 +86,20 @@ function AdminDashboardHome() {
                                 </div>
                             </Col>
                         </Row>
-                        <Row className="boxInfo p-2">
+                        <Row className="boxInfo p-2 ">
                             <Col >
                                 <div>
-                                    <p className="parCenter">Total Students: 99</p><br/>
-                                    <p className="parCenter">Total Students Enrolled: 99</p><br/>
-                                    <p className="parCenter">Total Students Drop: 99</p><br/>
+                                    <p className="parCenter">Total Students: 99</p><br/><br/>
+                                    <p className="parCenter">Total Students Resigned: 99</p><br/><br/>
+                                    <p className="parCenter">Total Students Present: 99</p>
                                 </div>
                             </Col>
                         </Row>
                     </Col>
                 </Row>
                 
-            </Container>
-            <Container className=" pt-5 d-flex  justify-content-center">
-                <Row className=" w-100">
+            
+                <Row className=" w-100 pt-5 justify-content-center">
                     <Col lg={5} sm={12} className=" shadow " >
                         <Row>
                             <Col className="boxTitle p-2">
@@ -60,12 +108,12 @@ function AdminDashboardHome() {
                                 </div>
                             </Col>
                         </Row>
-                        <Row className="boxInfo p-2">
+                        <Row className="boxInfo p-2 ">
                             <Col >
                                 <div>
-                                    <p className="parCenter">Total Subjects: 99</p><br/>
-                                    <p className="parCenter">Total Inactive Subjects : 99</p><br/>
-                                    <p className="parCenter">Total Active Subjects : 99</p><br/>
+                                    <p className="parCenter">Total Subjects: 99</p><br/><br/>
+                                    <p className="parCenter">Total Subjects Resigned: 99</p><br/><br/>
+                                    <p className="parCenter">Total Subjects Present: 99</p>
                                 </div>
                             </Col>
                         </Row>
@@ -76,17 +124,17 @@ function AdminDashboardHome() {
                     <Col lg={5} sm={12} className="shadow ">
                         <Row className="boxTitle p-2">
                             <Col>
-                                <div >
+                                <div>
                                     <h4>Course Information</h4>
                                 </div>
                             </Col>
                         </Row>
-                        <Row className="boxInfo p-2">
+                        <Row className="boxInfo p-2 ">
                             <Col >
-                                <div >
-                                    <p className="parCenter">Total Courses: 99</p><br/>
-                                    <p className="parCenter">Total Active Courses: 99</p><br/>
-                                    <p className="parCenter">Total Inactive Courses: 99</p><br/>
+                                <div>
+                                    <p className="parCenter">Total Course: 99</p> <br/><br/>
+                                    <p className="parCenter">Total Course Resigned: 99</p><br/><br/>
+                                    <p className="parCenter">Total Course Present: 99</p>
                                 </div>
                             </Col>
                         </Row>
